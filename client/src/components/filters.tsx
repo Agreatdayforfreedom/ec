d9c58a52eacfff36a4c8247b927aecc6da13518a
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2024-05-11 21:24:27 -0300

    remove filters

--- client/src/components/filters.tsx
+++ client/src/components/filters.tsx
@@ -1,28 +1,47 @@
-import { ElementRef, useRef } from 'react';
-import { Input } from './ui/input';
-import { ChevronRight } from 'lucide-react';
+import { ElementRef, useRef, useState } from 'react';
+import { ChevronRight, X } from 'lucide-react';
+import { useSearchParams } from 'react-router-dom';
+
+import { Input } from '@/components/ui/input';
 import useQueryParams from '@/hooks/use-query-params';
 
 export const Filters = () => {
 	const minRef = useRef<ElementRef<'input'>>(null);
 	const maxRef = useRef<ElementRef<'input'>>(null);
-	const [_, setParams] = useQueryParams();
+	const [_, setParams, deleteParam] = useQueryParams();
+	const [params] = useSearchParams();
+
+	const [min, setMin] = useState(params.get('min_price'));
+	const [max, setMax] = useState(params.get('max_price'));
 
 	const filterBy = () => {
-		let min_price = minRef?.current?.value;
-		let max_price = maxRef?.current?.value;
-		if (min_price && max_price) {
+		if (maxRef?.current?.value && minRef?.current?.value) {
+			let max_price = maxRef?.current?.value;
+			let min_price = minRef?.current?.value;
 			if (parseInt(min_price, 10) > parseInt(max_price, 10)) return;
 			setParams({ min_price, max_price });
+		}
+	};
+
+	const deleteFilters = () => {
+		deleteParam('min_price');
+		deleteParam('max_price');
+		if (minRef?.current && maxRef?.current) {
+			minRef.current.value = '';
+			maxRef.current.value = '';
+			setMin('');
+			setMax('');
 		}
 	};
 
 	return (
 		<div className="w-[25%] md:w-[15%] h-screen px-2 border-r border-magic-300">
 			<span className="font-bold">Price</span>
+
 			<div className="flex pl-1 mt-2 space-x-2">
 				<Input
 					ref={minRef}
+					defaultValue={min || ''}
 					placeholder="Min"
 					className="rounded-none h-6 bg-magic-550 border-magic-400 focus-visible:ring-offset-0 focus-visible:ring-magic-550"
 					type="number"
@@ -30,6 +49,7 @@ export const Filters = () => {
 				<Input
 					ref={maxRef}
 					placeholder="Max"
+					defaultValue={max || ''}
 					className="rounded-none h-6 bg-magic-550 border-magic-400 focus-visible:ring-offset-0 focus-visible:ring-magic-550"
 					type="number"
 				/>
@@ -40,6 +60,16 @@ export const Filters = () => {
 					<ChevronRight />
 				</button>
 			</div>
+			{maxRef?.current?.value && minRef?.current?.value && (
+				<div className="w-full flex justify-center mt-1">
+					<button
+						onClick={deleteFilters}
+						className="bg-magic-300 flex p-1 text-xs items-center font-bold hover:bg-magic-500 cursor-pointer"
+					>
+						Remove <X size={15} />
+					</button>
+				</div>
+			)}
 		</div>
 	);
 };
